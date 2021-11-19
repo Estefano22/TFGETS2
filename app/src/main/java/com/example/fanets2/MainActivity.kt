823cd36e41798ab8b1b8817fd5eb210cc6156f54
estefano <sandulescustefano8@gmail.com[Agit config --global user.email sandulescustefano8@gmail.comexitgit config --global user.email sandulescustefano8@gmail.com>
2021-11-19 14:01:48 +0100

    hecho hasta carritos

--- app/src/main/java/com/example/fanets2/MainActivity.kt
+++ app/src/main/java/com/example/fanets2/MainActivity.kt
@@ -3,7 +3,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
+import android.widget.EditText
+import androidx.core.widget.doAfterTextChanged
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +18,16 @@ class MainActivity : AppCompatActivity() {
         entrar.setOnClickListener {
             val intent = Intent(this,SegundoActivity::class.java)
             startActivity(intent)
+        }
+
+        var editText1 = findViewById<EditText>(R.id.nombreUsuario)
+        var editText2 = findViewById<EditText>(R.id.Contrasenia)
+
+        editText1.doAfterTextChanged {
+
+            if (it.isNullOrBlank()){
+
+            }
         }
 
     }
